3a021fc2a2c5b61894b57a4f3fc3117e771138f5
mbabaziviolet <mbabazijoy549@gmail.com>
2021-10-18 11:54:14 -0700

    assignment

---
+++ exercise3.py
@@ -0,0 +1,39 @@
+#values of x and y
+x=8
+y=2
+#multiple of x and y
+print(x*y)
+#exponential of x and y
+print(x**y)
+#division of x and y
+print(x/y)
+#floor division of x and y
+print(x//y)
+#this is question two (a)
+name = input("Enter your name :")
+print(name)
+#question two (b)
+name= input("Enter your name :")
+address= input("Enter your address :")
+number= input("Enter your telephone number :")
+print("Your details are:", name , address ,number, sep="\n")
+#this is question three (a)
+username= input("enter the name: ")
+print("user is "+ username)
+age=input("enter the age:")
+print( "age is"+ age)
+
+#question four
+from math import pi
+r=float(input("input the radius of the circle:"))
+print("The area of the circle with radius"+str(r)+"is:"+str(pi*r**2))
+#question three (b)
+z=2021
+m=2000
+print(z-m)
+#question five (a)
+#A python script 
+#is a language that python aiready knows
+#part (b)
+#when a python program is executed,it is translated into byte code.This byte code is sent to the python virtual machine
+#for further interpretation and execution
